5d39ea5afd64a47b919425208cd9299a4b150205
leandroalvesmachado@gmail.com <leandroalvesmachado@gmail.com>
2024-06-19 23:15:58 -0300

    adicionando tema e iniciando montagem layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+import '../../public/adminlte-3.2.0/plugins/jquery/jquery.min.js';
+import '../../public/adminlte-3.2.0/plugins/jquery-ui/jquery-ui.min.js';
+import '../../public/adminlte-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import '../../public/adminlte-3.2.0/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js';
+import '../../public/adminlte-3.2.0/dist/js/adminlte.js';
