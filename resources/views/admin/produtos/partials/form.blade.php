a4cb29bd65be28eb159043bd2a19a85e9392a5ab
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2024-07-08 00:26:41 -0300

    corrigindo texto de criação do produto e adicionando input de descrição

--- resources/views/admin/produtos/partials/form.blade.php
+++ resources/views/admin/produtos/partials/form.blade.php
@@ -16,10 +16,22 @@
                     name="nome"
                     id="nome"
                     class="form-control {{ $errors->has('nome') ? 'is-invalid' : '' }}"
-                    placeholder="Digite o nome do cliente"
-                    value="{{ old('nome', $cliente->nome ?? null) }}"
+                    placeholder="Digite o nome do produto"
+                    value="{{ old('nome', $produto->nome ?? null) }}"
                 />
                 <span class="text-danger">{{ $errors->first("nome") }}</span>
+            </div>
+            <div class="form-group">
+                <label>* Descrição</label>
+                <x-admin.form-input
+                    type="text"
+                    name="descricao"
+                    id="descricao"
+                    class="form-control {{ $errors->has('descricao') ? 'is-invalid' : '' }}"
+                    placeholder="Digite o descricao do produto"
+                    value="{{ old('descricao', $produto->descricao ?? null) }}"
+                />
+                <span class="text-danger">{{ $errors->first("descricao") }}</span>
             </div>
         </div>
     </div>
