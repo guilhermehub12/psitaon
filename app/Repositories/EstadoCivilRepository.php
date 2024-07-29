554722a9f612af08bb45360ecbab3c938a82826f
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2024-07-29 17:08:44 -0300

    repositories das novas tabelas criadas

---
+++ app/Repositories/EstadoCivilRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Repositories;
+
+use Exception;
+
+use App\Models\EstadoCivil;
+
+class EstadoCivilRepository extends BaseRepository
+{
+    protected $model = EstadoCivil::class;
+
+    public function paginate($paginate = 10, $orderBy = 'created_at', $sort = 'ASC', $filters = [])
+    {
+        try {
+            $query = $this->model->query();
+
+            if (count($filters) > 0) {
+            }
+
+            $query->orderBy($orderBy, $sort);
+
+            return $query->paginate($paginate);
+        } catch (Exception $e) {
+            return [];
+        }
+    }
+
+    public function selectOption()
+    {
+        try {
+            return $this->model
+                ->all()
+                ->sortBy('nome')
+                ->pluck('nome', 'id')
+                ->prepend('Escolha a opção', '');
+        } catch (Exception $e) {
+            return [
+                '' => $e->getMessage()
+            ];
+        }
+    }
+
+}
+
