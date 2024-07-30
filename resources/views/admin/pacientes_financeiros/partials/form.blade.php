f57d0321e19e7df6f0670ecd438fc08b4a766dc4
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2024-07-29 23:48:25 -0300

    campos de criação de financeiro

--- resources/views/admin/pacientes_financeiros/partials/form.blade.php
+++ resources/views/admin/pacientes_financeiros/partials/form.blade.php
@@ -4,14 +4,52 @@
         <div class="col-md-6">
             <div class="form-group">
                 <label>* Modalidade de Pagamento</label>
-                <x-admin.form-select
-                    name="modalidade_pagamento_id"
-                    id="modalidade_pagamento_id"
+                <x-admin.form-select name="modalidade_pagamento_id" id="modalidade_pagamento_id"
                     class="form-control {{ $errors->has('modalidade_pagamento_id') ? 'is-invalid' : '' }}"
-                    :options="$modalidades_pagamentos"
-                    selected="{{ old('modalidade_pagamento_id') }}"
-                />
-                <span class="text-danger">{{ $errors->first("modalidade_pagamento_id") }}</span>
+                    :options="$modalidades_pagamentos" selected="{{ old('modalidade_pagamento_id') }}" />
+                <span class="text-danger">{{ $errors->first('modalidade_pagamento_id') }}</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-6">
+        <div class="form-group">
+            <label>* Frequência de Pagamento</label>
+            <x-admin.form-select name="frequencias_pagamentos_id" id="frequencias_pagamentos_id"
+                class="form-control {{ $errors->has('frequencias_pagamentos_id') ? 'is-invalid' : '' }}"
+                :options="$frequencias_pagamentos" selected="{{ old('frequencias_pagamentos_id') }}" />
+            <span class="text-danger">{{ $errors->first('frequencias_pagamentos_id') }}</span>
+        </div>
+    </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+                <label>* Forma de Pagamento</label>
+                <x-admin.form-select name="formas_pagamentos_id" id="formas_pagamentos_id"
+                    class="form-control {{ $errors->has('formas_pagamentos_id') ? 'is-invalid' : '' }}"
+                    :options="$formas_pagamentos" selected="{{ old('formas_pagamentos_id') }}" />
+                <span class="text-danger">{{ $errors->first('formas_pagamentos_id') }}</span>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-group">
+                <label>* Status de Pagamento</label>
+                <x-admin.form-select name="status_pagamentos_id" id="status_pagamentos_id"
+                    class="form-control {{ $errors->has('status_pagamentos_id') ? 'is-invalid' : '' }}"
+                    :options="$status_pagamentos" selected="{{ old('status_pagamentos_id') }}" />
+                <span class="text-danger">{{ $errors->first('status_pagamentos_id') }}</span>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+                <label>* Status de Presença</label>
+                <x-admin.form-select name="status_presencas_id" id="status_presencas_id"
+                    class="form-control {{ $errors->has('status_presencas_id') ? 'is-invalid' : '' }}"
+                    :options="$status_presencas" selected="{{ old('status_presencas_id') }}" />
+                <span class="text-danger">{{ $errors->first('status_presencas_id') }}</span>
             </div>
         </div>
     </div>
