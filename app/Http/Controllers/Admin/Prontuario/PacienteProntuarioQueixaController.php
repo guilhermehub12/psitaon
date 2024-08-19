36383133af49df87c0620e957bde54f97b9418cf
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2024-08-19 00:15:21 -0300

    feat: criação da história e evolução da queixa do paciente

--- app/Http/Controllers/Admin/Prontuario/PacienteProntuarioQueixaController.php
+++ app/Http/Controllers/Admin/Prontuario/PacienteProntuarioQueixaController.php
@@ -3,9 +3,61 @@
 namespace App\Http\Controllers\Admin\Prontuario;
 
 use App\Http\Controllers\Controller;
+// use App\Http\Requests\Admin\Paciente\Prontuario\StoreProntuarioRequest;
+// use App\Http\Requests\Admin\Paciente\Prontuario\UpdateProntuarioRequest;
+use App\Models\Paciente;
+use App\Models\Prontuario\PacienteProntuarioQueixa;
+use App\Repositories\Prontuario\PacienteProntuarioQueixaRepository;
 use Illuminate\Http\Request;
 
-class PacienteProntuarioQueixa extends Controller
+class PacienteProntuarioQueixaController extends Controller
 {
-    //
+    public function __construct(
+        private PacienteProntuarioQueixaRepository $pacienteProntuarioQueixaRepository
+    ) {}
+
+     /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(
+        Paciente $paciente,
+        PacienteProntuarioQueixa $pacienteProntuarioQueixa
+    ) {
+        return view('admin.pacientes_prontuarios.paciente_prontuario_queixa.create', [
+            'paciente' => $paciente,
+            'pacienteProntuarioQueixa' => $pacienteProntuarioQueixa
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(
+        Request $request,
+        Paciente $paciente,
+        PacienteProntuarioQueixa $pacienteProntuarioQueixa
+    ) {
+        $result = $this->pacienteProntuarioQueixaRepository->store(
+            $pacienteProntuarioQueixa,
+            $request->except(['_token'])
+        );
+
+        if ($result === true) {
+            flash('Análise do risco cadastrada com sucesso!')->success();
+
+            return redirect()->route('admin.pacientes.prontuarios.index', $paciente);
+        }
+
+        flash('Erro ao cadastrar a análise do risco!')->error();
+
+        return redirect()->route('admin.pacientes.prontuarios_queixas.create', [
+            $paciente,
+            $pacienteProntuarioQueixa
+        ]);
+    }
 }
